22c4ddcbec6afbbbcba728f4ef42100bd1be261e
Sarah Allen <sarah@ultrasaurus.com>
2020-02-19 19:09:09 -0800

    wip

--- examples/no-async-main.rs
+++ examples/no-async-main.rs
@@ -4,9 +4,8 @@
 //! To run the example:
 //!
 //!     cargo run --example no-async-main
-use core::future::Future;
 use std::sync::{Arc, Mutex};
-use tokio::runtime::{Handle, Runtime};
+use tokio::runtime::Runtime;
 use tokio::sync::{mpsc, oneshot};
 use tokio::task::JoinHandle;
 use Command::*;
@@ -25,36 +24,22 @@ enum Response {
 
 struct RuntimeContext {
     runtime: Runtime,
-    cmd_handler: Option<JoinHandle<()>>,
-    tasks: Vec<i32>,
-}
-struct Commander {
-    context: Arc<Mutex<RuntimeContext>>,
+    cmd_handler: JoinHandle<()>,
+    tasks: Vec<JoinHandle<()>>,
 }
 
 #[derive(Clone)]
-struct Sender {
-    handle: Handle,
+struct Commander {
+    context: Arc<Mutex<RuntimeContext>>,
     cmd_tx: mpsc::Sender<(Command, oneshot::Sender<Response>)>,
 }
 
 impl Commander {
     pub fn new() -> Commander {
         let runtime = Runtime::new().expect("new tokio Runtime");
-        Commander {
-            context: Arc::new(Mutex::new(RuntimeContext {
-                runtime,
-                cmd_handler: None,
-                tasks: Vec::new(),
-            })),
-        }
-    }
-    pub fn connect(&self, ready_callback: impl Fn(Sender) -> () + Send + 'static) {
-        let mut context = self.context.lock().unwrap();
-
         let (cmd_tx, mut cmd_rx) = mpsc::channel::<(Command, oneshot::Sender<Response>)>(100);
         // Spawn a task to manage the counter
-        let handle = context.runtime.spawn(async move {
+        let cmd_handler = runtime.spawn(async move {
             let mut counter: u64 = 0;
             while let Some((cmd, response)) = cmd_rx.recv().await {
                 match cmd {
@@ -71,38 +56,46 @@ impl Commander {
             }
             println!("completed async task, originally spawned in connect");
         });
-        context.cmd_handler = Some(handle);
-        ready_callback(Sender::new(cmd_tx, context.runtime.handle().clone()));
+        Commander {
+            cmd_tx,
+            context: Arc::new(Mutex::new(RuntimeContext {
+                runtime,
+                cmd_handler,
+                tasks: Vec::new(),
+            })),
+        }
     }
-}
 
-impl Sender {
-    pub fn new(cmd_tx: mpsc::Sender<(Command, oneshot::Sender<Response>)>, handle: Handle) -> Self {
-        Sender { handle, cmd_tx }
-    }
-
-    pub fn send_command(&mut self, cmd: Command, f: impl Fn(Response) -> () + Send + 'static) {
+    pub fn send_command(&self, cmd: Command, f: impl Fn(Response) -> () + Send + 'static) {
+        println!("send_command, cmd_tx = {:?}", self.cmd_tx);
         let (resp_tx, resp_rx) = oneshot::channel::<Response>();
         let mut cmd_tx = self.cmd_tx.clone();
-        self.handle.spawn(async move {
+
+        let mut context = self.context.lock().unwrap();
+        let handle = context.runtime.spawn(async move {
             cmd_tx.send((cmd, resp_tx)).await.ok().unwrap();
             let res: Response = resp_rx.await.unwrap();
 
             println!("  => {:?}", res);
             f(res);
         });
+        context.tasks.push(handle);
     }
 } // impl Sender
 
 fn main() {
     let c = Commander::new();
-    c.connect(move |mut sender| {
-        println!("Yay!");
-        sender.send_command(Increment, |response| {
+    let cc = c.clone();
+    let ccc = c.clone();
+    cc.send_command(Increment, move |response| {
+        println!("  ==> received response {:?}", response);
+        ccc.send_command(Increment, move |response| {
             println!("  ==> received response {:?}", response);
-        })
+        });
     });
-
+    c.send_command(Increment, |response| {
+        println!("  ==> received response {:?}", response);
+    });
     let mut input = String::new();
     println!("press return to quit");
     std::io::stdin()
